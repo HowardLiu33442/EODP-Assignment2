11a6701d2e11327b0803d6439671421e1f348cc0
HowardLiu33442 <80027737+HowardLiu33442@users.noreply.github.com>
2021-05-08 19:35:57 +0800

    Update work from home.py

--- work from home.py
+++ work from home.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from numpy import arange
+
 plt.style.use('ggplot')
+
 work_from_home = pd.read_csv('Work from home arrangement.csv')
 work_from_home = work_from_home.loc[(work_from_home['Unnamed: 0'] == 'Childcare, Family considerations')|(work_from_home['Unnamed: 0']=='Flexible working arrangements')|(work_from_home['Unnamed: 0']=='Prefer to increase the amount of work done from home'), ['Unnamed: 0','Sex', 'Unnamed: 2']]
 male = []
@@ -13,6 +15,7 @@ for num in work_from_home['Unnamed: 2']:
 data = {'Male': male, 'Female': female}
 df = pd.DataFrame(data,columns=['Male','Female'], index = work_from_home['Unnamed: 0'].tolist())
 plt.style.use('ggplot')
-df.plot.barh()
-plt.title('Proportion of work from home arrangements between genders')
+df.plot.barh(alpha=0.7)
+plt.title('Proportion of work from home arrangements between genders', fontsize=12)
+plt.savefig('work from home arragement.png', format='png',dpi=200,bbox_inches='tight')
 plt.show()
